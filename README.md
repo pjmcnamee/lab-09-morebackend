459e0c820cb875d3ebaa450893cdea89559868ee
Patrick McNamee <pjmcnamee@comcast.net>
2018-09-21 09:12:47 -0700

    scaff

---
+++ README.md
@@ -0,0 +1,24 @@
+# lab-08-back-end
+```md
+# CodeFellows 301d38 Lab 8 back-end
+
+**Author**Nate Vail and Patrick McNamee
+**Version**: 1.0.0
+
+## Overview
+<!-- Refactor previous lab projects by storing collected API data into database that checks / updates as necessary. -->
+
+## Getting started
+<!-- Scaffold and populate necessary files, select js script previously built from earlier projects, create database, update js / schema.sq;=l / database as necessary to reach MVP -->
+
+## Architecture
+<!-- JavaScript, JSON, SQL, JQuery -->
+
+## Change log
+<!-- 
+200930TSEP18 - Repo created, scaffold built, .gitignore, .env, .eslintric.json, package-lock.json, package.json populated - Nate
+201000TSEP18 - Nate driving, Patrick nav-ing, select server.js file, build schema.sql.
+201015TSEP18 - Patrick driving, built JS - database link -->
+
+## Credits and Collaborations
+<!--  -->
---
+++ server.js
@@ -0,0 +1,167 @@
+'use strict';
+
+const express = require('express');
+const superagent = require('superagent');
+const cors = require('cors');
+const app = express();
+const pg = require('pg');
+require('dotenv').config();
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+
+client.on('error', err => console.error(err));
+
+app.use(cors());
+
+
+app.get('/location', getLocation)
+
+app.get('/weather', getWeather);
+
+app.get('/movies', getMovies);
+
+app.get('/yelp', getYelp);
+
+const PORT = process.env.PORT || 3000;
+
+function deleteByLocationId(table, city) {
+  const SQL =  `DELETE from ${table} WHERE location_id=${city};`;
+  return client.query(SQL);
+}
+
+// constructor function for geolocation - called upon inside the request for location
+function Location(query, result) {
+  this.search_query = query;
+  this.formatted_query = result.body.results[0].formatted_address,
+  this.latitude = result.body.results[0].geometry.location.lat,
+  this.longitude = result.body.results[0].geometry.location.lng
+  this.created_at = Date.now();
+}
+
+function getLocation(request, response) {
+  Location.lookupLocation({
+    tableName: Location.tableName,
+    query: request.query.data,
+    cacheHit: function(result) {
+      response.send(result);
+    },
+    cacheMiss: function () {
+      const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GOOGLE_API_KEY}`;
+      return superagent.get(url)
+        .then(result => {
+          const location = new Location(this.query, result);
+          location.save()
+            .then(location => response.send(location));
+        })
+        .catch(error => handleError(error));
+    }
+  })
+}
+
+Location.lookupLocation = (location) => {
+  const SQL = `SELECT * FROM locations WHERE search_query=$1;`;
+  const values = [location.query];
+
+  return client.query(SQL,values)
+    .then(result => {
+      if (result.rowCount > 0) {
+        location.cacheHit(result.rows[0]);
+      } else {
+        location.cacheMiss();
+      }
+    })
+    .catch(console.error);
+}
+
+Location.prototype = {
+  save: function() {
+    const SQL = `INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING id;`;
+    const values = [
+      this.search_query,
+      this.formatted_query,
+      this.latitude,
+      this.longitude,
+    ];
+    return client.query(SQL, values)
+      .then(result=> {
+        this.id = result.rows[0].id;
+        return this;
+      });
+  }
+};
+
+
+
+//send request to DarkSkys API and gets data back, then calls on Weather function to display data
+function getWeather(request, response) {
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+  return superagent.get(url)
+
+    .then( result => {
+      const weatherSummaries = result.body.daily.data.map( day => {
+        return new Weather(day);
+      })
+      response.send(weatherSummaries)
+      console.log(weatherSummaries);
+    })
+    .catch( error => handleError(error, response));
+}
+
+function Weather(day) {
+  this.time = new Date(day.time * 1000).toString().slice(0, 15);
+  this.forecast = day.summary;
+}
+
+// Yelp Api request
+function getYelp(request, response) {
+  const url = `https://api.yelp.com/v3/businesses/search?location=${request.query.data.search_query}`;
+
+  superagent.get(url)
+    .set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
+    .then(result => {
+      const businessSummaries = result.body.businesses.map(data => {
+        return new Yelp(data);
+      });
+      response.send(businessSummaries);
+      console.log(businessSummaries);
+    })
+    .catch( error => handleError(error, response));
+}
+
+function Yelp(data) {
+  this.name = data.name;
+  this.image_url = data.image_url;
+  this.price = data.price;
+  this.rating = data.rating;
+  this.url = data.url;
+}
+
+function getMovies(request, response) {
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_API_KEY}&query=${request.query.data.search_query}`;
+  return superagent.get(url)
+
+    .then(result => {
+      const moviesSummaries = result.body.results.map(movies => {
+        return new MoviesData(movies);
+      })
+      response.send(moviesSummaries);
+    })
+    .catch( error => handleError(error, response));
+}
+
+function MoviesData(movies) {
+  this.title = movies.title;
+  this.overview = movies.overview;
+  this.average_votes = movies.vote_average;
+  this.total_votes = movies.vote_count;
+  this.image_url = `https://image.tmdb.org/t/p/w200_and_h300_bestv2${movies.poster_path}`;
+  this.popularity = movies.popularity;
+  this.released_on = movies.release_date;
+}
+
+function handleError(err, res) {
+  console.error(err);
+  if (res) res.status(500).send('Sorry, something went wrong');
+}
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
